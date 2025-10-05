8ef7960949d90568a2a0f66d18cfd6ff9cffb435
Esteban <estebanquinterob10@gmail.com>
2025-10-06 01:29:00 +0200

    desarrollo de estructura de componentes Header y Main

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProductCard from './components/ProductCard';
-import ProductList from './components/ProductList';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-blue-500 text-white p-6 rounded-lg">
+      <Header />
+  
+      <div className="bg-blue-500 text-white p-6 rounded-lg m-5" >
         <h1 className="text-3xl font-bold">¡Tailwind funciona! 🚀</h1>
         <h2 className='bg-amber-300 text-amber-950'>Estoy orgulloso de haber llegado a una solucion</h2>
       </div>
-      <ProductCard />
-      <ProductList />
-      <Navbar />
+      <Main />
+      
     </>
   )
 }
